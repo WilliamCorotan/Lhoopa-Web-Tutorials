59d97a44db957d0a046aa9163983b435a9ba46a6
William Corotan <williamcorotan@gmail.com>
2023-04-25 18:11:06 +0800

    started mini project

---
+++ php/lux/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SAMPLE WEBSITE</title>
+    <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar bg-body-tertiary border-bottom">
+            <div class="container-fluid">
+                <span class="navbar-brand mb-0 h1">Navbar</span>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
