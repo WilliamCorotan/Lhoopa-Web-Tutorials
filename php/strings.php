d78e0587f23d53a72945462e307507aba55508b5
William Corotan <williamcorotan@gmail.com>
2023-04-25 17:13:57 +0800

    strings

---
+++ php/strings.php
@@ -0,0 +1,27 @@
+<?php 
+/* 
+
+    STRING FUNCTIONS
+        substr(string $string, int $offset, ?int $length = null): string
+            return a portion of a string        
+        strlen(string $string): int
+            return the length of the string
+        strpos(string $haystack, string $needle, int $offset = 0): int|false
+            return the index the first matched element
+        strrpos(string $haystack, string $needle, int $offset = 0): int|false
+            return the index of the last matched element
+        trim(string $string, string $characters = " \n\r\t\v\x00"): string
+            return a string with striped whitespace
+        strtoupper(string $string): string
+            return string in uppercases
+        strtolower(string $string): string
+            return string in lowercases
+        ucwords(string $string, string $separators = " \t\r\n\f\v"): string
+            return a string with uppercase in each word
+        str_replace(array|string $search, array|string $replace, string|array $subject,int &$count = null): string|array
+            return string with the replaced value of a word
+        is_string(mixed $value): bool
+            check if value is a string
+        
+            */  
+?>
