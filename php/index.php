ccdb659d92266a03d80a5b2a077d47a25ba0ebeb
William Corotan <williamcorotan@gmail.com>
2023-04-25 14:13:40 +0800

    data types, variables and constants

--- php/index.php
+++ php/index.php
@@ -1,3 +1,23 @@
 <?php 
+    /* 
+        DATA TYPES:
+            Strings
+            Integers
+            Floats
+            Boolean
+            Arrays
+            Objects
+            NULL
+            Resources
+    */
 
+    $output  = "hello world!";
+    $num1 = 2;
+    $float = 4.4;
+    $bool = true;
+
+    // Defining constants
+
+    define('CONSTANT_ONE', "Meaning of the variable");
+    echo $output;
 ?>
