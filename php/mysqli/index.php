ff5ff6e259b0c79e82030661abca92cca0c201ba
William Corotan <williamcorotan@gmail.com>
2023-04-26 16:39:06 +0800

    fixed add button link

--- php/mysqli/index.php
+++ php/mysqli/index.php
@@ -39,7 +39,7 @@ mysqli_close($con);
                 </div>
             </div>
         </section>
-        <button type="button" class="btn btn-success position-fixed end-0 bottom-0 m-2">Add new post</button>
+        <a href="add.php" class="btn btn-success position-fixed end-0 bottom-0 m-2">Add new post</a>
     </main>
 
 <?php include('includes/footer.php')?>
