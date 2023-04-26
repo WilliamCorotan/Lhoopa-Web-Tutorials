693ed6ce6d18ab16bafb38cc0889a62903d8a592
William Corotan <williamcorotan@gmail.com>
2023-04-26 16:34:20 +0800

    added partials and components

---
+++ php/mysqli/includes/navbar.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="navbar" id="navbarColor03">
+      <ul class="navbar-nav ">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
