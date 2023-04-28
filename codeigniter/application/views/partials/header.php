0a976358e889942c1d9642c898412000fed89d6e
William Corotan <williamcorotan@gmail.com>
2023-04-28 11:31:24 +0800

    refactored header and footer

--- codeigniter/application/views/partials/header.php
+++ codeigniter/application/views/partials/header.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://bootswatch.com/5/lux/bootstrap.min.css">
+    <title> <?= $title . " | " . APPLICATION_NAME?> </title>
 </head>
 <body>
-    
+<header>
+    <?php $this->view("components/navbar")?>
+</header>
+<main class="container">
+   
