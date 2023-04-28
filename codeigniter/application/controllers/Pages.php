969779233a3cd66c7885f11d64024e84e9dfe620
William Corotan <williamcorotan@gmail.com>
2023-04-28 10:07:46 +0800

    added partials and configured Pages controller

---
+++ codeigniter/application/controllers/Pages.php
@@ -0,0 +1,32 @@
+<?php 
+
+class Pages extends CI_Controller{
+
+public function view($page = 'home'){
+
+    /**
+     * check if the pages exists or not
+     * 
+     */ 
+    if(!file_exists(APPPATH."views/pages/$page.php")){
+        show_404();
+    }
+
+    /**
+     * this assigns the page name to the title of the page
+     * 
+     */
+    $data['title'] = ucfirst($page);
+
+    /**
+     * 
+     * this loads the header, footer partials and the pages that is passed
+     * !!order does matter!!
+     * 
+     */
+    $this->load->view('partials/header');
+    $this->load->view("partials/pages/$page", $data);
+    $this->load->view('partials/footer');
+}
+}
+?>
