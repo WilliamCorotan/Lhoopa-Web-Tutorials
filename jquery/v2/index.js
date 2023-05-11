43c24e299b429ea850b3dbad338ab14cab011c5a
William Corotan <williamcorotan@gmail.com>
2023-05-11 11:15:44 +0800

    added comments and minor hotfixes

--- jquery/v2/index.js
+++ jquery/v2/index.js
@@ -1,76 +1,136 @@
 $(function () {
-    let leftOperand = 0;
-    let rightOperand = 0;
+    // Variable Declaration
+    let leftOperand = '';
+    let rightOperand = '';
     let operator = '';
-    let result = 0;
+    let result = '';
 
+    
+/**
+ * Takes the left operand as parameter 
+ */
     function evaluate(num1) {
+
+        /**
+         * returns a function that takes right hand operand and operation as parameter and returns the result
+         */
         return function operation(num2, operation) {
-            if(operation === "+"){
-                const res = num1 + num2;
-                return res;
-            }
-            else if(operation === "-"){
-                const res = num1 - num2;
-                return res;
-            }
-            else if(operation === "*"){
-                const res = num1 * num2;
-                return res;
-            }
-            else if(operation === "/"){
-                const res = num1 / num2;
-                console.log('num1: ' +num1 + typeof num1)
-                console.log('num2: ' +num2 + typeof num2)
-                console.log('res: ' +res)
-                return res;
-            }
-            else{
-                return result;
+            switch (operation) {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    return num1 / num2;
+                default:
+                    return result;
             }
         }
     }
 
+    /**
+     * Click event for number buttons
+     */ 
     $('.number').on('click', function(){
         $('.calculator-screen').val('')
 
+        /**
+         * Stores value to the right operand if an operator exists
+         */
         if(operator){
-            console.log('right')
             const $rightOperand = $(this).val();
-            rightOperand += ($rightOperand)
-            $('.calculator-screen').val(parseFloat(rightOperand))
+            rightOperand += ($rightOperand)     
+            $('.calculator-screen').val(rightOperand)
             return
         }
-        console.log('left')
+        
+        /**
+         * Stores value to the left operand 
+         */
         const $leftOperand = $(this).val();
         leftOperand += ($leftOperand)
-        $('.calculator-screen').val(parseFloat(leftOperand))
+        $('.calculator-screen').val(leftOperand)
     })
     
+    /**
+     * Click event for operator buttons 
+     */
     $('.operator').on('click', function(){
-        if(leftOperand && rightOperand){
-            const eval = evaluate(parseFloat(leftOperand));
-            result = eval(parseFloat(rightOperand), operator);
-            $('.calculator-screen').val(parseFloat(result))
-            leftOperand = result
-            rightOperand = 0
+
+        /**
+         * Checks if left and right operands has values
+        */ 
+       if(leftOperand && rightOperand){
+           const eval = evaluate(parseFloat(leftOperand));
+           result = eval(parseFloat(rightOperand), operator);
+           $('.calculator-screen').val(result)
+           leftOperand = result.toString()
+           rightOperand = ''
         }
-        if($(this).val === "="){
-            $('.calculator-screen').val(parseFloat(result))
-            leftOperand = result
-            rightOperand = 0
-        }
+        
+        /**
+         * Checks if the operation is equals (=)
+        */
+       if($(this).val == "="){
+           $('.calculator-screen').val(result)
+           leftOperand = result.toString()
+           rightOperand = ''
+        }        
+
         const $operator = $(this).val();
         operator = $operator;
+        $('.operator-display').val(operator)
     })
 
+    /**
+     * Click event for all clear button
+     */
     $('.all-clear').on('click', function(){
         $('.calculator-screen').val('');
-        rightOperand = 0;
-        leftOperand = 0;
+        rightOperand = '';
+        leftOperand = '';
         operator = '';
-        result = 0;
-        console.log('emptyy')
+        result = '';
+    })
+
+    /**
+     * Click event for decimal button
+     */
+    $('.decimal').on('click', function() {
+
+        /**
+         * Early returns if the left operand has an existing period and has no operator
+         */
+        if(leftOperand.includes('.') && !operator){
+            return
+        }
+
+        /**
+         * Early Returns if the right operand has an existing period
+         */
+        if(rightOperand.includes('.')) {
+            return
+        }
+
+        /**
+         * Appends a period to the right operand if an operator exists
+         */
+        if(operator){
+            const $decimal = $(this).val();
+            rightOperand += ($decimal)
+            $('.calculator-screen').val(rightOperand)
+            return
+        }
+
+        /**
+         * Appends a period to the left operand 
+         */
+        const $decimal = $(this).val();
+        leftOperand += ($decimal)
+        $('.calculator-screen').val(leftOperand)
+
     })
 
 });
